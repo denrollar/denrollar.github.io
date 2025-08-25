7ea3a7b8ca6d4ec8438cfd039d818bf84788086a
denrollar <okdenrollar@gmail.com>
2025-08-25 21:35:16 +0500

    fix scrobbler

--- js/now-playing.js
+++ js/now-playing.js
@@ -1,14 +1,10 @@
 const API_KEY = "ec5d88052f9f80ecfa6031c129e93d8f";
 const USER = "denrollar";
-
 const output = document.getElementById("now-playing");
 
-
 function fetchNowPlaying() {
-
   const old = document.getElementById("lfm-jsonp");
   if (old) old.remove();
-
 
   window.__lfmCallback = function (data) {
     try {
@@ -18,26 +14,27 @@ function fetchNowPlaying() {
       const isPlaying = track["@attr"] && track["@attr"].nowplaying === "true";
 
       output.textContent = isPlaying
-        ? ${artist} — ${name}
-        : "Сейчас ничего не играет";
+        ? artist + " – " + name
+        : "";
     } catch (e) {
-      output.textContent = "Ошибка загрузки";
+      output.textContent = "";
       console.error(e);
     }
   };
 
   const url =
-    https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks +
-    &user=${encodeURIComponent(USER)} +
-    &api_key=${encodeURIComponent(API_KEY)} +
-    &format=json&limit=1&callback=__lfmCallback&_=${Date.now()};
+    "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks" +
+    "&user=" + encodeURIComponent(USER) +
+    "&api_key=" + encodeURIComponent(API_KEY) +
+    "&format=json&limit=1" +
+    "&callback=__lfmCallback&_=" + Date.now();
 
   const s = document.createElement("script");
   s.id = "lfm-jsonp";
   s.src = url;
-  s.onerror = () => (output.textContent = "Ошибка сети/JSONP");
+  s.onerror = () => (output.textContent = "");
   document.body.appendChild(s);
 }
 
 fetchNowPlaying();
-setInterval(fetchNowPlaying, 15000);
+setInterval(fetchNowPlaying, 30000);
