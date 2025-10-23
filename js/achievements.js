70840432d42098c8a11c26d79957d224edad657a
denrollar <okdenrollar@gmail.com>
2025-10-24 00:41:40 +0500

    555

--- js/achievements.js
+++ js/achievements.js
@@ -54,10 +54,10 @@ containerAch2.innerHTML = htmlAch2;
 /* П3 */
 
 const ach3 = [
-  { name: "Посмотрено кино: 100 шт", xtext: "Фильмы, сериалы и мультфильмы", date: "23 ОКТЯБРЯ 2025", image: "resources/images/icons/gallery.svg" },
-  { name: "Прослушано альбомов: 100 шт", xtext: "От начала до конца", date: "23 ОКТЯБРЯ 2025", image: "resources/images/icons/gallery.svg" },
-  { name: "Прочитано книг: 5 шт", xtext: "От начала до конца", date: "ВЕСНА 2024", image: "resources/images/icons/gallery.svg" },
-  { name: "Завершено видеоигр: 5 шт", xtext: "Исключаются просмотренные", date: "2023", image: "resources/images/icons/gallery.svg" },
+  { name: "Посмотрено кино: 100 шт", xtext: "Фильмы, сериалы и мультфильмы", date: "23 ОКТЯБРЯ 2025", image: "resources/images/icons/cinema.webp" },
+  { name: "Прочитано книг: 5 шт", xtext: "От начала до конца", date: "ВЕСНА 2024", image: "resources/images/icons/book.webp" },
+  { name: "Прослушано альбомов: 100 шт", xtext: "От начала до конца", date: "23 ОКТЯБРЯ 2025", image: "resources/images/icons/album.webp" },
+  { name: "Завершено видеоигр: 5 шт", xtext: "Исключаются просмотренные", date: "2023", image: "resources/images/icons/games.webp" },
   { name: "God of War: 100% завершено", xtext: "ПК · 2018", date: "2023", image: "resources/images/covers/games/godofwar2018.webp" },
 ];
 
