31c5592a6778262b95831a9e1e39ea1e9d4f47b7
denrollar <okdenrollar@gmail.com>
2025-10-26 14:10:28 +0500

    fix
    fixed logo for app

--- js/achievements.js
+++ js/achievements.js
@@ -28,7 +28,7 @@ containerAch1.innerHTML = htmlAch1;
 
 const ach2 = [
   { name: "Отжимания: 15 повторений", xtext: "Лучший результат за 1 подход", date: "22 ОКТЯБРЯ 2025", image: "resources/images/icons/medal-15.webp" },
-  { name: "Приседания: 50 повторений", xtext: "Лучший результат за 1 подход", date: "23 ОКТЯБРЯ 2025", image: "resources/images/icons/medal-100.webp" },
+  { name: "Приседания: 100 повторений", xtext: "Лучший результат за 1 подход", date: "23 ОКТЯБРЯ 2025", image: "resources/images/icons/medal-100.webp" },
 ];
 
 const containerAch2 = document.getElementById("ach2");
