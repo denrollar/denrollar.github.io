c66bb0d01ed1b1d7fc5aaf687375f3b900b6f879
denrollar <okdenrollar@gmail.com>
2025-11-15 23:01:34 +0500

    fix

--- js/achievements.js
+++ js/achievements.js
@@ -58,8 +58,6 @@ const ach3 = [
   { name: "Прочитано книг: 5 шт", xtext: "От начала до конца", date: "ВЕСНА 2024", image: "resources/images/icons/book.webp" },
   { name: "Прослушано альбомов: 100 шт", xtext: "От начала до конца", date: "23 ОКТЯБРЯ 2025", image: "resources/images/icons/album.webp" },
   { name: "Завершено видеоигр: 25 шт", xtext: "Исключаются просмотренные", date: "2025", image: "resources/images/icons/games.webp" },
-  { name: "GTA: San Andreas - 100% завершено", xtext: "ПК · 2004", date: "04 НОЯБРЯ 2025", image: "resources/images/icons/s2.webp" },
-  { name: "God of War - 100% завершено", xtext: "ПК · 2018", date: "ОКТЯБРЬ 2023", image: "resources/images/icons/s2.webp" },
 ];
 
 const containerAch3 = document.getElementById("ach3");
@@ -82,3 +80,29 @@ ach3.forEach(item => {
 
 containerAch3.innerHTML = htmlAch3;
 
+/* П4 */
+
+const ach4 = [
+  { name: "GTA: San Andreas - 100% завершено", xtext: "ПК · 2004", date: "04 НОЯБРЯ 2025", image: "resources/images/icons/s2.webp" },
+  { name: "God of War - 100% завершено", xtext: "ПК · 2018", date: "ОКТЯБРЬ 2023", image: "resources/images/icons/s2.webp" },
+];
+
+const containerAch4 = document.getElementById("ach4");
+let htmlAch4 = '';
+
+ach4.forEach(item => {
+  htmlAch4 += `
+    <div class="block-achievements">
+      <div class="hidden-block-1">
+        <div class="section-title">${item.name}</div>
+       <div class="ach-section-title">${item.xtext}</div>
+        <div class="section-subtitle">${item.date}</div>
+      </div>
+      <div class="icon7">
+        <img src="${item.image}" class="icon7" alt="${item.name}">
+      </div>
+    </div>
+  `;
+});
+
+containerAch4.innerHTML = htmlAch4;
